de70994d803b6524ae708983584f5c6fff4ecdcb
JON882846 <evgeniy.2888@gmail.com>
2022-06-05 17:26:36 +0500

    Trynumbertwo

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -9,30 +9,40 @@ const val THREE_DAY = 259200
 
 
 fun main() {
-    agoToText(870)
-
+    agoToText(3601)
 }
+
 
 fun agoToText (
     second: Int,
 
     ){
     when (second){
-        in 0..Minute ->  println("был (а) в сети $second только что")
+        in 0..Minute ->  println("был (а) в сети только что")
 
-        in Minute + 1..Hour -> println("был (а) в сети  минут назад")
+        in Minute + 1..Hour ->
+        {
+            val result = second/60
+            when(result) {
+                1, 21, 31, 41, 51 -> println("был (а) в сети $result минуту назад")
+                2, 3, 4, 22, 23, 24, 32, 34, 42, 43, 44, 52, 53, 54 -> println("был (а) в сети $result минуты назад")
+                else -> println("был (а) в сети $result минут назад")
+            }
+        }
 
-        in Hour + 1..Day  -> println("был (а) в сети  часов назад")
+        in Hour + 1..Day  ->
+        {
+            val result = second/3600
+            when(result) {
+                1, 21 -> println("был (а) в сети $result час назад")
+                2, 3, 4, 22, 23 -> println("был (а) в сети $result часа назад")
+                else -> println("был (а) в сети $result часов назад")
+            }
+        }
+        in Day ..TWO_DAY  -> println("был (а) в сети  сегодня")
 
-        in Day ..TWO_DAY  -> println("был (а) в сети  часов назад")
+        in TWO_DAY ..THREE_DAY  -> println("был (а) в сети вчера")
 
-        in TWO_DAY ..THREE_DAY  -> println("был (а) в сети  часов назад")
-
-        else -> println("в сети не появлялся")
-
-
+        else -> println("был (а) в сети давно")
     }
-
-
 }
-
